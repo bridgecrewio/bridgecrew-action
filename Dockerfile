0d0c695d296071244251abb3af1d47aebadfd725
barak <schosterbarak@gmail.com>
2020-05-23 23:36:50 +0300

    add problem matcher and simplify entrypoint

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ RUN pip install -U bridgecrew
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
+COPY bridgecrew-problem-matcher.json /usr/local/lib/bridgecrew-problem-matcher.json
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,22 @@
 #!/bin/bash
 
+matcher_path=`pwd`/bridgecrew-problem-matcher.json
+cp /usr/local/lib/bridgecrew-problem-matcher.json "$matcher_path"
+echo "::add-matcher::bridgecrew-problem-matcher.json"
+
+[[ ! -z "$INPUT_CHECK" ]] && CHECK_FLAG="--check $INPUT_CHECK"
+[[ ! -z "$INPUT_SKIP_CHECK" ]] && SKIP_CHECK_FLAG="--skip-check $INPUT_SKIP_CHECK"
+[[ ! -z "$INPUT_QUIET" ]] && QUIET_FLAG="--quiet"
+[[ ! -z "$INPUT_SOFT_FAIL" ]] && SOFT_FAIL_FLAG="--soft-fail"
+
 NONE=none
-SUPPRESS=${SOFT_FAIL}
-OUTPUT=${OUTPUT}
-DIRECTORY=${DIRECTORY}
-FILE=${FILE}
+
 API_KEY=${API_KEY_VARIABLE}
 
 CMD_STR="bridgecrew -o $OUTPUT"
-if [[ $FILE == $NONE ]] && [[ $DIRECTORY == $NONE ]]; then
-  echo "file or directory must be provided"
-  exit 1
-elif [[ $FILE != $NONE ]] && [[ $DIRECTORY != $NONE ]]; then
-  echo "cannot provide file and directory, chose only one"
-  exit 1
+
+if [ -n "$API_KEY_VARIABLE" ]; then
+  bridgecrew --bc-api-key $API_KEY_VARIABLE --branch ${GITHUB_REF##*/} --repo-id $GITHUB_REPOSITORY -d $INPUT_DIRECTORY $CHECK_FLAG $SKIP_CHECK_FLAG $QUIET_FLAG $SOFT_FAIL_FLAG
+  else
+  bridgecrew -d $INPUT_DIRECTORY $CHECK_FLAG $SKIP_CHECK_FLAG $QUIET_FLAG
 fi
-if [ $SUPPRESS == "true" ]; then
-  CMD_STR="$CMD_STR -s"
-fi
-if [ $FILE != $NONE ]; then
-  CMD_STR="$CMD_STR -f $FILE"
-fi
-if [ $DIRECTORY != $NONE ]; then
-  CMD_STR="$CMD_STR -d $DIRECTORY"
-fi
-if [ -n "$API_KEY" ]; then
-  CMD_STR="$CMD_STR --bc-api-key $API_KEY --branch ${GITHUB_REF##*/} --repo-id $GITHUB_REPOSITORY"
-fi
-echo "running bridgecrew with command: $CMD_STR"
-echo $CMD_STR | sh
