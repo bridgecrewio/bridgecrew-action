23734922afa3a01604e484e2f15eaadb71c968c8
guy sulema <suli@virtue.im>
2020-07-01 18:54:27 +0300

    Change the source to env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 matcher_path=`pwd`/bridgecrew-problem-matcher.json
 cp /usr/local/lib/bridgecrew-problem-matcher.json "$matcher_path"
 echo "::add-matcher::bridgecrew-problem-matcher.json"
+export BC_SOURCE=githubActions
 
 [[ ! -z "$INPUT_CHECK" ]] && CHECK_FLAG="--check $INPUT_CHECK"
 [[ ! -z "$INPUT_SKIP_CHECK" ]] && SKIP_CHECK_FLAG="--skip-check $INPUT_SKIP_CHECK"
@@ -16,7 +17,7 @@ API_KEY=${API_KEY_VARIABLE}
 CMD_STR="bridgecrew -o $OUTPUT"
 
 if [ -n "$API_KEY_VARIABLE" ]; then
-  bridgecrew --bc-api-key $API_KEY_VARIABLE --branch ${GITHUB_REF##*/} --repo-id $GITHUB_REPOSITORY -d $INPUT_DIRECTORY $CHECK_FLAG $SKIP_CHECK_FLAG $QUIET_FLAG $SOFT_FAIL_FLAG --source githubActions
+  bridgecrew --bc-api-key $API_KEY_VARIABLE --branch ${GITHUB_REF##*/} --repo-id $GITHUB_REPOSITORY -d $INPUT_DIRECTORY $CHECK_FLAG $SKIP_CHECK_FLAG $QUIET_FLAG $SOFT_FAIL_FLAG
   else
   bridgecrew -d $INPUT_DIRECTORY $CHECK_FLAG $SKIP_CHECK_FLAG $QUIET_FLAG
 fi
