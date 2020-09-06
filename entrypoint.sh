c6b9582c163cdb31748f6e625ee930faf9663bd2
Barak Schoster Goihman <barak@bridgecrew.io>
2020-09-06 12:33:45 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ export BC_SOURCE=githubActions
 [[ ! -z "$INPUT_CHECK" ]] && CHECK_FLAG="--check $INPUT_CHECK"
 [[ ! -z "$INPUT_SKIP_CHECK" ]] && SKIP_CHECK_FLAG="--skip-check $INPUT_SKIP_CHECK"
 [[ ! -z "$INPUT_QUIET" ]] && QUIET_FLAG="--quiet"
-[[ ! -z "$INPUT_SOFT_FAIL" ]] && SOFT_FAIL_FLAG="--soft-fail"
+[[ ! -z "$INPUT_SOFT-FAIL" ]] && SOFT_FAIL_FLAG="--soft-fail"
 [[ ! -z "$EXTERNAL_CHECKS_DIR" ]] && EXTERNAL_CHECKS_DIR_FLAG="--external-checks-dir $EXTERNAL_CHECKS_DIR"
 [[ ! -z "$INPUT_OUTPUT_FORMAT" ]] && OUTPUT_FLAG="-o $INPUT_OUTPUT_FORMAT"
 
