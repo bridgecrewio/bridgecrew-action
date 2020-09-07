1088aacf063c65c0f0794a3d0795db2ea28787d2
Barak Schoster Goihman <barak@bridgecrew.io>
2020-09-07 16:21:56 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ NONE=none
 API_KEY=${API_KEY_VARIABLE}
 
 CMD_STR="bridgecrew -o $OUTPUT"
-GIT_BRANCH=$(git symbolic-ref --short HEAD)
+GIT_BRANCH=$(git branch | sed -n '/\* /s///p')
 echo "b"
 echo $GITHUB_REPOSITORY
 cmd="bridgecrew --bc-api-key $API_KEY_VARIABLE --branch $GIT_BRANCH --repo-id $GITHUB_REPOSITORY -d $INPUT_DIRECTORY $CHECK_FLAG $SKIP_CHECK_FLAG $QUIET_FLAG $SOFT_FAIL_FLAG $EXTERNAL_CHECKS_DIR_FLAG $OUTPUT_FLAG"
