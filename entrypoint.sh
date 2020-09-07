fbb44407e1b72598311a50b3b97e2912a785cd36
Barak Schoster Goihman <barak@bridgecrew.io>
2020-09-07 16:32:28 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,14 +11,14 @@ export BC_SOURCE=githubActions
 [[ ! -z "$INPUT_SOFT-FAIL" ]] && SOFT_FAIL_FLAG="--soft-fail"
 [[ ! -z "$EXTERNAL_CHECKS_DIR" ]] && EXTERNAL_CHECKS_DIR_FLAG="--external-checks-dir $EXTERNAL_CHECKS_DIR"
 [[ ! -z "$INPUT_OUTPUT_FORMAT" ]] && OUTPUT_FLAG="-o $INPUT_OUTPUT_FORMAT"
-
+echo "::set-env name=BRANCH_NAME::$(echo ${GITHUB_HEAD_REF})"
 
 NONE=none
 
 API_KEY=${API_KEY_VARIABLE}
 
 CMD_STR="bridgecrew -o $OUTPUT"
-GIT_BRANCH=$(git branch | sed -n '/\* /s///p')
+GIT_BRANCH=$BRANCH_NAME
 echo "b"
 echo $GITHUB_REPOSITORY
 cmd="bridgecrew --bc-api-key $API_KEY_VARIABLE --branch $GIT_BRANCH --repo-id $GITHUB_REPOSITORY -d $INPUT_DIRECTORY $CHECK_FLAG $SKIP_CHECK_FLAG $QUIET_FLAG $SOFT_FAIL_FLAG $EXTERNAL_CHECKS_DIR_FLAG $OUTPUT_FLAG"
